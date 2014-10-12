15fafaad70551eb8990056848e8795d4d7f959c6
Travis Rigg <rigg.travis@gmail.com>
2014-10-12 19:49:56 -0400

    Added header information to courses controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,12 +1,17 @@
+# Public: The functions that access and manipulate the information
+#   in the courses table.
+
 class CoursesController < ApplicationController
   before_action :set_course, only: [:show, :edit, :update, :destroy]
 
+  # Public: Lists all courses
   # GET /courses
   # GET /courses.json
   def index
     @courses = Course.all
   end
 
+  # Public: Outputs the information relating to a single course.
   # GET /courses/1
   # GET /courses/1.json
   def show
@@ -16,15 +21,19 @@ class CoursesController < ApplicationController
     end
   end
 
+  # Public: Generates a new course, but does not add it to the
+  #   database.
   # GET /courses/new
   def new
     @course = Course.new
   end
 
+  # Public: Allows a user to change the values of an existing course.
   # GET /courses/1/edit
   def edit
   end
 
+  # Public: Adds the information for a course to the database.
   # POST /courses
   # POST /courses.json
   def create
@@ -41,6 +50,7 @@ class CoursesController < ApplicationController
     end
   end
 
+  # Public: Saves any changes made to a course to the database.
   # PATCH/PUT /courses/1
   # PATCH/PUT /courses/1.json
   def update
@@ -55,6 +65,7 @@ class CoursesController < ApplicationController
     end
   end
 
+  # Public: Removes the information for a course from the database.
   # DELETE /courses/1
   # DELETE /courses/1.json
   def destroy
@@ -66,11 +77,13 @@ class CoursesController < ApplicationController
   end
 
   private
+    # Internal: creates a variable containing information for a course.
     # Use callbacks to share common setup or constraints between actions.
     def set_course
       @course = Course.find(params[:id])
     end
 
+    # Internal: Enforces sane data manipulation.
     # Never trust parameters from the scary internet, only allow the white list through.
     def course_params
       params.require(:course).permit(:name, :term, :subject, :listing, :user_id)
