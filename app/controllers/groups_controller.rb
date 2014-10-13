426b7573e0a17f0edbddc2b812a23d45454fc505
Travis Rigg <rigg.travis@gmail.com>
2014-10-12 20:35:23 -0400

    Added header information to the group controller.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,12 +1,17 @@
+# Public: This class contains all of the functions that will be
+#   allowed to mannipulate and access  data in the group table.
+
 class GroupsController < ApplicationController
   before_action :set_group, only: [:show, :edit, :update, :destroy]
 
+  # Public: Lists all entries.
   # GET /groups
   # GET /groups.json
   def index
     @groups = Group.all
   end
 
+  # Public: Gives the information for a single entry.
   # GET /groups/1
   # GET /groups/1.json
   def show
@@ -21,15 +26,18 @@ class GroupsController < ApplicationController
     end
   end
 
+  # Public: Creates a new group object.
   # GET /groups/new
   def new
     @group = Group.new
   end
 
+  # Public: Allows the user to make changes to a group object.
   # GET /groups/1/edit
   def edit
   end
 
+  # Public: Instantiates a group object, and adds it to the database
   # POST /groups
   # POST /groups.json
   def create
@@ -46,6 +54,7 @@ class GroupsController < ApplicationController
     end
   end
 
+  # Public: Saves any changes to the database.
   # PATCH/PUT /groups/1
   # PATCH/PUT /groups/1.json
   def update
@@ -60,6 +69,7 @@ class GroupsController < ApplicationController
     end
   end
 
+  # Public: Removes a group entry from the database.
   # DELETE /groups/1
   # DELETE /groups/1.json
   def destroy
@@ -71,11 +81,13 @@ class GroupsController < ApplicationController
   end
 
   private
+    # Internal: Creates a variable that contains the information for a group.
     # Use callbacks to share common setup or constraints between actions.
     def set_group
       @group = Group.find(params[:id])
     end
 
+    # Internal: Enforce data access sanity.
     # Never trust parameters from the scary internet, only allow the white list through.
     def group_params
       params.require(:group).permit(:user_id, :course_id)
