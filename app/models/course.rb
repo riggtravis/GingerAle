e0883457f31451f36da8cd7b5f8fb4f1cf522671
Travis Rigg <rigg.travis@gmail.com>
2014-10-12 20:50:31 -0400

    Added header information to the course model.

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,8 +1,14 @@
+# Public: This is the description of the course table and how it
+#   relates to the other tables.
+
 class Course < ActiveRecord::Base
   has_many :groups, dependent: :destroy
   has_many :users
   belongs_to :user
   
+  # Public: This checks to see if the user that owns the course is the
+  #   same as another user.
+  # user_check - the user to be tested against the owner.
   # Create a scope that checks the user.
   scope :user_is, ->(user_check) { where ("user_id = ?", user_check) }
 end
