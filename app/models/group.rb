5f5cb144a0ebf223bb6c3eec9f5d773cf3701219
Travis Rigg <rigg.travis@gmail.com>
2014-10-12 20:53:53 -0400

    Added header information to group model.

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,6 @@
+# Public: This is a description of the group table and how it relates to
+#   the other tables.
+
 class Group < ActiveRecord::Base
   has_many :users, dependent: :destroy
   has_many :ratings, dependent: :destroy
@@ -6,6 +9,13 @@ class Group < ActiveRecord::Base
   belongs_to :user
   belongs_to :course
   
+  # PubliC: Check to see if a user is the same as the group's owner.
+  # user_check - the user to be tested against the group owner.
   scope :user_is, ->(user_check) { where ("user_id = ?", user_check) }
+  
+  # Public: check to see if a course is the same as the course a group
+  #   belongs to.
+  # course_check -  the course to be tested against the course that the
+  #                 group belongs to.
   scope :course_is, ->(course_check) {where ("course_id = ?", user_check) }
 end
