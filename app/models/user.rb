bf5be3efccd91fa57924f742a2af9b3efc9c03a9
Travis Rigg <rigg.travis@gmail.com>
2014-10-12 20:58:24 -0400

    Added header information to the user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
+# Public: This is the description of the
+#   user table and how it relates to the
+#   other tables.
+
 class User < ActiveRecord::Base
   has_many :ratings, dependent: :destroy
   has_many :courses, dependent: :destroy
